f3de073e506f1f2e9702712b29b3003ee87d26af
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-11-02 14:39:50 +0530

    Add controller code

--- employee-server/src/main/java/com/dtech/employee_server/contorller/EmployeeController.java
+++ employee-server/src/main/java/com/dtech/employee_server/contorller/EmployeeController.java
@@ -1,15 +1,39 @@
 package com.dtech.employee_server.contorller;
 
+import com.dtech.employee_server.entity.Employee;
+import com.dtech.employee_server.service.EmployeeService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/employee" , produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/api/employee", produces = MediaType.APPLICATION_JSON_VALUE)
 public class EmployeeController {
-    @GetMapping("/get")
-    public String get(){
-        return "Deeepak";
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+    
+    @PostMapping("/create")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+
+    @GetMapping("/all")
+    public List<Employee> getAllEmployee() {
+        return employeeService.getAllEmployee();
+    }
+
+    @PutMapping("/update/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+        return employeeService.deleteEmplpoyee(id);
     }
 }
