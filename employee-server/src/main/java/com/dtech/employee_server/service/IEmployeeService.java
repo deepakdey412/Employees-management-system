ed9b00434685d3a95b4e6030852cef2fa19ec046
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-11-02 14:39:13 +0530

    Update Service code

--- employee-server/src/main/java/com/dtech/employee_server/service/IEmployeeService.java
+++ employee-server/src/main/java/com/dtech/employee_server/service/IEmployeeService.java
@@ -5,33 +5,44 @@ import com.dtech.employee_server.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class IEmployeeService  implements  EmployeeService{
+public class IEmployeeService implements EmployeeService {
+
     private final EmployeeRepository employeeRepository;
 
     public IEmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
-
     @Override
     public Employee createEmployee(Employee employee) {
-        return null;
+        return employeeRepository.save(employee);
     }
 
     @Override
     public List<Employee> getAllEmployee() {
-        return List.of();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee updateEmployee(Long id, Employee employee) {
-        return null;
+        Optional<Employee> existingEmployee = employeeRepository.findById(id);
+        if (existingEmployee.isPresent()) {
+            Employee emp = existingEmployee.get();
+            emp.setName(employee.getName());
+            emp.setEmail(employee.getEmail());
+            emp.setDepartment(employee.getDepartment());
+            return employeeRepository.save(emp);
+        } else {
+            throw new RuntimeException("Employee not found with id " + id);
+        }
     }
 
     @Override
     public String deleteEmplpoyee(Long id) {
-        return "";
+        employeeRepository.deleteById(id);
+        return "Employee deleted successfully with id " + id;
     }
 }
